bb6cca9b0747c9599e771a69b2201896d962d9ef
Marc-Antoine Couture <coumarc9@outlook.com>
2017-07-16 23:10:47 -0400

    Adding filter strategy interface

---
+++ src/proc_hydrophone/filter_strategy/IFilterStrategy.h
@@ -0,0 +1,19 @@
+//
+// Created by coumarc9 on 7/16/17.
+//
+
+#ifndef PROC_HYDROPHONE_IFILTERSTRATEGY_H
+#define PROC_HYDROPHONE_IFILTERSTRATEGY_H
+
+#include <geometry_msgs/Quaternion.h>
+#include <provider_hydrophone/PingMsg.h>
+
+namespace proc_hydrophone
+{
+    class IFilterStrategy
+    {
+        virtual geometry_msgs::QuaternionConstPtr Process(std::vector<provider_hydrophone::PingMsgConstPtr> pings) = 0;
+    };
+}
+
+#endif //PROC_HYDROPHONE_IFILTERSTRATEGY_H
